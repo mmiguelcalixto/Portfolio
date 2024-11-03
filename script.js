17faff393e5c555f1c939a473ab04c06e6fbe865
Miguel <miguelcalixtomendes@gmail.com>
2024-11-03 19:31:13 -0300

    feat: modo escuro e typing effect

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-const aboutMe = document.querySelector("#about-me")
-const skills = document.querySelector("#skills")
-const projects = document.querySelector("#projects")
-const contact = document.querySelector("#contact")
+const body = document.body;
+const aboutSection = document.querySelector("#about-section");
+const skillsSection = document.querySelector("#skills-section");
+const greyContainer = document.querySelector("#grey-container");
+const projectInfo = document.querySelectorAll(".project-info");
+const darkModeSwitch = document.querySelector(".theme-checkbox");
 
-function scrollToPosition() {
-    aboutMe.addEventListener("click", () => {
-        scrollTo(0,500)
-    })
+const texts = [
+    { text: "Miguel", elementId: "first-h1" },
+    { text: "Calixto", elementId: "second-h1" },
+    { text: "Desenvolvedor de software", elementId: "main-text-p" }
+];
+const speed = 80;
+let textIndex = 0;
+let charIndex = 0;
+
+function typeWriter() {
+    if (textIndex >= texts.length) return;
+
+    const { text, elementId } = texts[textIndex];
+    const element = document.getElementById(elementId);
+
+    if (charIndex < text.length) {
+        element.innerHTML += text.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        textIndex++;
+        charIndex = 0;
+        setTimeout(typeWriter, speed);
+    }
 }
+
+window.onload = function() {
+    typeWriter();
+};
+
+darkModeSwitch.addEventListener("change", () =>  {
+    body.classList.toggle("dark-mode");
+    aboutSection.classList.toggle("dark-mode");
+    skillsSection.classList.toggle("dark-mode2");
+    greyContainer.classList.toggle("dark-mode");
+    
+    projectInfo.forEach(i => {
+        i.classList.toggle("dark-mode2");
+    });
+})
+
+
 
 
